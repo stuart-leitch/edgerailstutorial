feab733ba37dad2248f613dd2d59e80b7628eb35
BuildTools <unconfigured@null.spigotmc.org>
2021-10-11 13:27:38 +0100

    section4

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "articles#index"
+  
+  get "/articles", to: "articles#index"
 
-  # Almost every application defines a route for the root path ("/") at the top of this file.
-  # root "articles#index"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
